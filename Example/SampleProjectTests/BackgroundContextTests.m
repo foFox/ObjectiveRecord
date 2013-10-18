055cc114829b10e73305b4257cbc275d17b52a85
robertlis <robert.lis2@mail.dcu.ie>
2013-10-18 15:25:14 +0100

    Added text suite for NSManagedObjectContext category

---
+++ Example/SampleProjectTests/BackgroundContextTests.m
@@ -0,0 +1,27 @@
+//
+//  MappingsTests.m
+//  SampleProject
+//
+//  Created by Robert Lis on 5/28/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import <ObjectiveRecord/CoreDataManager.h>
+
+SPEC_BEGIN(BackgroundContextTests)
+
+describe(@"NSManagedObjectContext", ^{
+
+    context(@"on call to performInBackground", ^{
+        
+        it(@"throws assertion error when not called from the main thread", ^{
+            
+    
+        });
+        
+    });
+    
+});
+
+SPEC_END
